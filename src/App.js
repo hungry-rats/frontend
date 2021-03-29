674af138e81ccafeacaa70782836db959985ace2
mgtz505 <gutierrez.marcusc@gmail.com>
2021-03-29 18:34:08 -0400

    v_1 pass at Landing Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import react from 'react';
 import './App.css';
 import { Route, Link } from 'react-router-dom';
 import { Row, Col, Container } from 'react-bootstrap';
+import LandingCarousel from './landing-page-components/LandingCarousel';
 // import LandingNavBar from './landing-page-components/LandingNavBar';
 
 function App() {
 	return (
 		<div className='App'>
-			<Route path='/' exact render={() => <LandingNavBar />} />
+			{/* <Route path='/' exact render={() => <LandingNavBar />} /> */}
+			<LandingCarousel/>
 		</div>
 	);
 }
--- src/landing-page-components/LandingCarousel.js
+++ src/landing-page-components/LandingCarousel.js
@@ -1,21 +1,217 @@
-
-import React, {useState, useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
-
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Carousel, Form, Jumbotron } from "react-bootstrap";
+import "./LandingCarousel.css";
 
 export default function LandingCarousel() {
-	return <div></div>;
-  
+  return (
 
-// Need to add this to my HTML
+	<div className="container">
+		<Button variant="info" className="logoIcon">PLACEHOLDER FOR ICON</Button>
+		<Button variant="info" className="userButton">
+			<span>USER</span>
+		</Button>
+		<div className="spacer"></div>
+		<div className="welcomeBar">
+			<h1>Welcome to Tasty</h1>
+		</div>
+		<div className="carouselGrid">
+			<div className="carouselOne">
+				<Carousel fade>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="First slide"
+					/>
+					<Carousel.Caption>
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="Second slide"
+					/>
 
-// <div id="container">
-//   <!-- Your component will be rendered here. -->
-// </div>
+					<Carousel.Caption>
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="Third slide"
+					/>
 
+					<Carousel.Caption>
+					<h3>Third slide label</h3>
+					<p>
+						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+					</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				</Carousel>
+			</div>
+			{/* <div>SPACER</div> */}
+			<div className="carouselTwo">
+				<Carousel fade>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="First slide"
+					/>
+					<Carousel.Caption>
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="Second slide"
+					/>
 
-const [current, setCurrent] = React.useState(0)
+					<Carousel.Caption>
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="Third slide"
+					/>
 
+					<Carousel.Caption>
+					<h3>Third slide label</h3>
+					<p>
+						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+					</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				</Carousel>
+			</div>
+			{/* <div>SPACER</div> */}
+			<div className="carouselThree">
+				<Carousel fade>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="First slide"
+					/>
+					<Carousel.Caption>
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="Second slide"
+					/>
+
+					<Carousel.Caption>
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="Third slide"
+					/>
+
+					<Carousel.Caption>
+					<h3>Third slide label</h3>
+					<p>
+						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+					</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				</Carousel>
+			</div>
+			{/* <div>SPACER</div> */}
+			<div className="carouselFour">
+				<Carousel fade>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="First slide"
+					/>
+					<Carousel.Caption>
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="Second slide"
+					/>
+
+					<Carousel.Caption>
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+					className="d-block w-100"
+					src="https://picsum.photos/200/300"
+					alt="Third slide"
+					/>
+
+					<Carousel.Caption>
+					<h3>Third slide label</h3>
+					<p>
+						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+					</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				</Carousel>
+			</div>
+
+		<div className = "linkToFeed">
+		<Jumbotron>
+			<h1>Welcome To Tasty</h1>
+			<p>
+				This is a simple hero unit, a simple jumbotron-style component for calling
+				extra attention to featured content or information.
+			</p>
+			<p>
+				<Button variant="info">Visit my Feed</Button>
+			</p>
+		</Jumbotron>
+		</div>
+	
+		<div className = "signUpForm">
+			<Form>
+				<Form.Group controlId="formGroupEmail">
+					<h2> Sign Up!</h2>
+					<Form.Label>Email address</Form.Label>
+					<Form.Control type="email" placeholder="Enter email" />
+				</Form.Group>
+				<Form.Group controlId="formGroupPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Password" />
+				</Form.Group>
+			</Form>
+		</div>
+
+		</div>
+	</div>
+  );
 }
