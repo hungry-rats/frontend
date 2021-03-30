c358538d18ffdeede954606b70df2bad79e769aa
DylanDrechsel <DrechselDylan@gmail.com>
2021-03-30 16:25:54 -0400

    sign up and page change to feed component when complete

--- src/App.js
+++ src/App.js
@@ -7,18 +7,34 @@ import LandingCarousel from './landing-page-components/LandingCarousel';
 import NavBar from './global-components/NavBar';
 import Home from './home-components/Home';
 import Details from './home-components/Details';
+import { atom, useRecoilState, useRecoilValue } from "recoil"
+import { userState as userStateAtom } from './landing-page-components/LandingCarousel'
 
 function App() {
-	return (
-		<div className='App'>
-			<Route path='/' exact render={() => <LandingCarousel />} />
-			{/* <LandingCarousel/> */}
-			{/* <NavBar /> */}
-			{/* turnary will go here LOGED IN OR NOT */}
-			<Route path='/home' exact render={() => <Home />} />
-			<Route path='/:id' exact component={Details} />
-		</div>
-	);
+
+	const [user, setUser] = useRecoilState(userStateAtom)
+
+	console.log(user)
+
+	if (user === false) {
+		return (
+			<div className='App'>
+				<Route path='/' exact render={() => <LandingCarousel />} />
+
+				<Route path='/home' exact render={() => <Home />} />
+				{/* <LandingCarousel/> */}
+				{/* <NavBar /> */}
+				{/* turnary will go here LOGED IN OR NOT */}
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<Route path='/' exact render={() => <Home />} />
+				<Route path='/:id' exact component={Details} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RecoilRoot } from "recoil"
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
