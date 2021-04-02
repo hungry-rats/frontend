a3151a4b7d48f1c9cba515de0b4b49dca3410e74
mremsha <michaelremsha@gmail.com>
2021-04-02 14:23:08 -0400

    New Recipie inputs set state and push to back end

--- src/home-components/RecipeCreate.js
+++ src/home-components/RecipeCreate.js
@@ -3,60 +3,85 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { tokenState as tokenStateAtom } from '../landing-page-components/LandingCarousel';
 import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { Button, Form } from 'react-bootstrap';
+import './RecipeCreate.css';
 
 const RecipeCreate = () => {
-    const [token, setToken] = useRecoilState(tokenStateAtom);
-    const [recipe, setRecipe] = useState({
-        title: "TESTTESTTESTTEST",
-        author: "user 1",
-    })
+	const [token, setToken] = useRecoilState(tokenStateAtom);
+	const [recipe, setRecipe] = useState({
+		title: 'TESTTESTTESTTEST',
+		author: 'user 1',
+	});
 
-    console.log(token)
+	console.log(token);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+	const handleSubmit = (event) => {
+		event.preventDefault();
 
-        axios({
-					url: `http://localhost:8000/recipes`,
-					method: 'POST',
-					data: recipe,
-					headers: {
-						Authorization:
-							`Bearer ${token}`,
-					},
-				});}
-
-    function handleInformation(event) {
-		setRecipe({
-			title: event.target.value,
-			author: event.target.value,
-			inspiredBy: event.target.value,
-            allergies: event.target.value,
-            image: event.target.value,
-            ingredients: [event.target.value],
-            directions: [event.target.value]
+		axios({
+			url: `http://localhost:8000/recipes`,
+			method: 'POST',
+			data: recipe,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
 		});
+	};
+
+	// function handleInformation(event) {
+	// 	setRecipe({
+	// 		title: event.target.value,
+	// 		author: event.target.value,
+	// 		inspiredBy: event.target.value,
+	// 		allergies: event.target.value,
+	// 		image: event.target.value,
+	// 		ingredients: [event.target.value],
+	// 		directions: [event.target.value],
+	// 	});
+	// }
+
+	function handleInput(event) {
+		// event.preventDefault();
+		const input = { ...recipe };
+		input[event.target.id] = event.target.value;
+		setRecipe(input);
+		// console.log(event.target.id);
 	}
 
-   return (
+	// function submittest() {
+	// 	event.preventDefault();
+	// 	console.log(recipe);
+	// }
+
+	return (
 		<div>
 			<h1>New recipie</h1>
 			<Form onSubmit={handleSubmit}>
-				<Form.Group controlId='title'>
+				<Form.Group controlId='title' onChange={handleInput}>
 					<Form.Label>Title</Form.Label>
 					<Form.Control placeholder='Enter title' />
 				</Form.Group>
-				<Form.Group controlId='inspiredBy'>
+				<Form.Group controlId='inspiredBy' onChange={handleInput}>
 					<Form.Label>Inspired By</Form.Label>
 					<Form.Control placeholder='Enter Origin' />
 				</Form.Group>
-				<Form.Group controlId='imageSource'>
+				<Form.Group controlId='image' onChange={handleInput}>
 					<Form.Label>Image Link</Form.Label>
 					<Form.Control placeholder='Enter link to the image' />
 				</Form.Group>
-				<Form.Group controlId='ingridients'>
-					<Form.Label>Image Link</Form.Label>
-					<Form.Control placeholder='Enter Ingridients' />
+				<Form.Group controlId='ingridients' onChange={handleInput}>
+					<Form.Label>Ingridients</Form.Label>
+					<Form.Control
+						placeholder='Enter Ingridients'
+						style={{ height: '200px' }}
+					/>
+				</Form.Group>
+				<Form.Group controlId='directions' onChange={handleInput}>
+					<Form.Label>Directions</Form.Label>
+					<Form.Control
+						placeholder='Enter Instructions'
+						style={{ height: '200px' }}
+					/>
 				</Form.Group>
 				<Button variant='primary' type='submit'>
 					Submit
@@ -68,4 +93,3 @@ const RecipeCreate = () => {
 };
 
 export default RecipeCreate;
-
